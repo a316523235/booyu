0950da3d51e621082973dd2b3a78339bc6e3c92d
huangzhijian <huangzhijian@linggan.com>
2017-05-11 18:31:09 +0800

    base gBanker price

--- routes/gbanker.js
+++ routes/gbanker.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var basePrice = 175.00;
-var thisAddPrice = 175.00;
+var investPrice = 175.00;
 var request = require('request');
 
 /* GET home page. */
@@ -17,7 +17,7 @@ router.get('/', function(req, res, next) {
 
   var r = request(options, function (error, response, body) {
 	if (!error && response.statusCode == 200) {
-	  console.log(body)
+	  console.log(body);
 	  res.json({ title: 'g-banker info', basePrice: basePrice, result: body});
 	}
   });
@@ -31,5 +31,14 @@ router.post('/', function(req, res, next) {
   }
   res.json({ rt: 1, title: 'g-banker info', basePrice: basePrice, oldBasePrice: oldBasePrice });
 });
+
+function checkPrice(gbankerPrice) {
+	gbankerPrice = Math.floor(gbankerPrice);
+	if(gbankerPrice < baseprice) {
+		
+	} else {
+
+	}
+}
 
 module.exports = router;
